6c2c7e192d0d324f30801ee68bc786122dda47e4
Jake Halpern <jhalpern@purdue.edu>
2024-11-20 14:47:09 -0500

    specifying more physical dipole dimensions as opposed to number of dipoles based on Thea discussion

--- run_optimizer_simple.py
+++ run_optimizer_simple.py
@@ -8,7 +8,7 @@ import numpy as np
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 ###### set this to wherever you'd like all the outputs from this script to go
-run_dir = os.path.join(script_dir, '../outputs/nInboard_scan_10_31_24')
+run_dir = os.path.join(script_dir, '../outputs/2024_11_20_larger_gaps')
 
 # this is the name of the script you're going to replace the parameters for and run
 script_template_name = 'optimizer_template_simple.py' ### use this for HBT calcs (no TF dofs)
@@ -17,29 +17,37 @@ script_name = 'optimizer.py' ### what you want the script saved as in outputs fo
 
 ### Simulation parameters ###
 # TF coils parameters
-n_tf = 4                       # number of TF coils per half field period
+n_tf = 5                       # number of TF coils per half field period
 num_fixed = 1                  # number of TF coil currents to fix during combined optimization
 vc_flag = False                # Use finite beta when running HBT_optimizer_template
 field_on_axis = 1.0            # B field at magnetic axis in Tesla
-fixed_geo = False               # fix the TF geometry
-TF_radius = 0.6         # range of TF coil radii to optimize over, put in increasing order
-    
-# Windowpane parameters
-axisymmetric = True            # use axisymmetric vessel for dipoles
-max_dipole_field = False         # [T], set by engineering constraints. If false, no current limit
-win_nPhi = 11                    # Number of windows per half period, toroidal dimension
-win_nTheta = 16              # Number of windows, poloidal dimension
-win_size = 8                   # Number of grid cells/window, both dimensions
-win_gap = 2                    # Number of grid cells between adjacent windows
-uneven_grid = False
-nInboard = win_nTheta - 2   # number of smaller dipoles on inboard side
-nOutboard = 2               # number of large dipoles on outboard side
-theta_inboard_start = 0.2   # starting point for smaller dipoles, normalized such that 0 -> 1 = 0 -> 2pi
-theta_inboard_end = 0.8     # ending point for smaller dipoles
+fixed_geo = True               # fix the TF geometry
+TF_radius = 0.4         # range of TF coil radii to optimize over, put in increasing order
 
 # Vacuum Vessel
-VV_a = 0.24                   # minor radius of vacuum vessel
+VV_a = 0.27                   # minor radius of vacuum vessel
 VV_R0 = 1.0                  # major radius of vacuum vessel
+
+# Windowpane parameters
+axisymmetric = True          # use axisymmetric vessel for dipoles
+max_dipole_field = False     # [T], set by engineering constraints. If false, no current limit
+fil_to_fil_distance = 0.05   # [m], distance between filaments, based on quoted value from Thea
+win_size_over_gap = 2        # Ratio of dipole width to gap distance, dipole size will be approximately win_size_over_gap * fil_to_fil_distance
+uneven_grid = False           # allow uneven poloidal spacing in dipole grid
+theta_inboard_start = 0.15   # starting point for smaller dipoles, normalized such that 0 -> 1 = 0 -> 2pi
+theta_inboard_end = 0.85     # ending point for smaller dipoles
+
+# Fill these in based on above parameters (don't touch unless you know what you're doing)
+win_gap = 2                    # Number of grid cells between adjacent windows - must be even
+win_size = win_gap * win_size_over_gap    # Number of grid cells per window
+inner_circum = 2 * np.pi * (VV_R0 - VV_a)
+win_nPhi = int(np.round(inner_circum / fil_to_fil_distance / (win_size_over_gap + 1) / 2 / 2))   # Number of windows per half period, toroidal dimension
+if uneven_grid:
+    nInboard =  int(np.round(2 * np.pi * VV_a * (theta_inboard_end - theta_inboard_start) /  fil_to_fil_distance / (win_size_over_gap + 1)))   # number of smaller dipoles on inboard side        
+else:
+    nInboard =  int(np.round(2 * np.pi * VV_a /  fil_to_fil_distance / (win_size_over_gap + 1)))   # number of smaller dipoles on inboard side        
+nOutboard = 2               # number of large dipoles on outboard side
+win_nTheta = nInboard + nOutboard  # Number of windows, poloidal dimension
 
 # Plasma Surface
 plas_nPhi = 64                 # toroidal points on plasma surface
@@ -68,9 +76,10 @@ if dpi != 100:
 if uneven_grid == True:
     extra = extra + f"_uneven_grid"
 
+num = str(input("Run number for directory naming: "))
 extra = extra + str(input("Anything extra to add to pathname? If none, just press enter: "))
 
-run_dir = os.path.join(run_dir, f'{eq_name}/Bt{field_on_axis}_Bd{max_dipole_field}_ntf{n_tf}_np{win_nPhi}_nt{win_nTheta}{extra}/')
+run_dir = os.path.join(run_dir, f'{eq_name}/{num}_Bt{field_on_axis}_Bd{max_dipole_field}_ntf{n_tf}_np{win_nPhi}_nt{win_nTheta}{extra}/')
 
 ###### Change directory
 os.makedirs(run_dir, exist_ok=True)
